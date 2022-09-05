b2a48c3d2a24e2f0cd1e6b59391781c6d858f0ed
JacekHacking <Jacekhacking@mac.com>
2022-09-05 15:55:17 -0600

    react location installed routes setup and working links WIP

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
-import './App.css';
-import Nav from "./components/Nav/Nav.js"
+import "./App.css";
+import Nav from "./components/Nav/Nav.js";
 import LandingPage from "./components/LandingPage/LandingPage";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Shopping from "./components/Shopping/Shopping";
-import {useState} from "react";
+import { useState } from "react";
+import { Outlet, ReactLocation, Router } from "@tanstack/react-location";
 
 function App() {
+  const [page, handlePageChange] = useState();
+  const location = new ReactLocation();
 
-    const [page, handlePageChange] = useState()
+  // const renderPage = () => {
+  //   switch (page) {
+  //     case "About Me":
+  //       return <AboutMe />;
 
-    const renderPage = () => {
-        switch (page) {
+  //     case "Portfolio":
+  //       return <Portfolio />;
 
-            case 'About Me':
-                return <AboutMe/>;
+  //     case "Shopping":
+  //       return <Shopping />;
 
-            case 'Portfolio' :
-                return<Portfolio/>
-
-            case 'Shopping' :
-                return <Shopping/>
-
-            default:
-                return <LandingPage page ={page} handlePageChange = {handlePageChange} />
-        }
-    }
+  //     default:
+  //       return <LandingPage page={page} handlePageChange={handlePageChange} />;
+  //   }
+  // };
 
   return (
-    <div className="App">
-      <body className={'flow'}>
-      <Nav page ={page} handlePageChange = {handlePageChange} />
-      {renderPage(page)}
-      </body>
-    </div>
+    <>
+      <Router
+        location={location}
+        routes={[
+          {
+            path: "/",
+            element: () =>
+              import("./components/LandingPage/LandingPage").then((module) => (
+                <module.default />
+              )),
+          },
+          {
+            path: "/portfolio",
+            element: () =>
+              import("./components/Portfolio/Portfolio").then((module) => (
+                <module.default />
+              )),
+          },
+          {
+            path: "/about",
+            element: () =>
+              import("./components/AboutMe/AboutMe").then((module) => (
+                <module.default />
+              )),
+          },
+          {
+            path: "/shop",
+            element: () =>
+              import("./components/Shopping/Shopping").then((module) => (
+                <module.default />
+              )),
+          },
+        ]}
+      >
+        <Nav page={page} handlePageChange={handlePageChange} />
+        <Outlet />
+      </Router>
+    </>
   );
 }
 
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,84 +1,82 @@
-import React from 'react';
+import React from "react";
 import styles from "./LandingPage.module.css";
 import Selfie from "../UI/Images/About me.JPG";
 import PortfolioPic from "../UI/Images/Portfolio pic.png";
-import tShirtPic from "../UI/Images/shopping pic.jpg"
+import tShirtPic from "../UI/Images/shopping pic.jpg";
 import AboutButton from "./AboutButton";
 
-
 const LandingPage = (props) => {
-    return (
-        <div>
-            {/*header*/}
-            <div style={{position: 'relative', zIndex: '3'}}>
-                <p style={{padding: '1em', textAlign: 'center', zIndex: '3'}}>
-                    Welcome! My name is Keaton Hacking. I'm an illustration artist.
-                    I specialize in digital media.
-                </p>
-            </div>
+  return (
+    <div>
+      {/*header*/}
+      <div style={{ position: "relative", zIndex: "3" }}>
+        <p style={{ padding: "1em", textAlign: "center", zIndex: "3" }}>
+          Welcome! My name is Keaton Hacking. I'm an illustration artist. I
+          specialize in digital media.
+        </p>
+      </div>
 
-
-            {/*about me component*/}
-            <div className={styles['landing-page-card']}>
-                <img src={Selfie} alt=""/>
-                <div style={{margin: '1em', zIndex: '2'}}>
-                    <p className={styles['landing-page-paragraph']}>Learn more about the artist and the inspiration behind his work</p>
-                    <button onClick={() => props.handlePageChange('About Me')}
-                            className={styles['landing-page-card-button']}>
-                        <AboutButton/>
-                    </button>
-                </div>
-            </div>
-
-
-            {/*Portfolio component*/}
-            <div className={styles['landing-page-card']}>
-                <div style={{zIndex: '2'}}>
-                    <p className={styles['landing-page-paragraph']}> Check out pieces done using a mix of charcoal, and digital media. </p>
-                    <button onClick={() => props.handlePageChange('Portfolio')}
-                            className={styles['landing-page-card-button']}>
-                        <section
-                            className={'gradient-pink-dark-blue'}>
-                            Portfolio
-                        </section>
-                    </button>
-                </div>
-                <img style={{margin: '1em'}} src={PortfolioPic} alt=""/>
-            </div>
-
-
-            {/*shopping component*/}
-
-            <div className={styles['landing-page-card']}>
-                <img src={tShirtPic} alt=""/>
-                <div style={{margin: '1em', zIndex: '2'}}>
-                    <p className={styles['landing-page-paragraph']}>Shop prints, stickers, clothing inquire about custom pieces!
-                    </p>
-                    <button
-                        onClick={() => props.handlePageChange('Shopping')}
-                        className={styles['landing-page-card-button']}>
-
-                        <section
-                            className={'gradient-green-light-blue'}>
-                            SHOP
-                        </section>
-                    </button>
-                </div>
-            </div>
-
-
-
-            {/*----------------------------------------------------------------------------------------*/}
-            {/*                       landing page blog elements position absolute                     */}
-            {/*----------------------------------------------------------------------------------------*/}
-
-            <div className={styles['landing-page-blob-1']}></div>
-            <div className={styles['landing-page-blob-2']}></div>
-            <div className={styles['landing-page-blob-3']}></div>
-            <div className={styles['landing-page-blob-4']}></div>
-            <div className={styles['landing-page-blob-5']}></div>
-            <div className={styles['landing-page-blob-6']}></div>
+      {/*about me component*/}
+      <div className={styles["landing-page-card"]}>
+        <img src={Selfie} alt="" />
+        <div style={{ margin: "1em", zIndex: "2" }}>
+          <p className={styles["landing-page-paragraph"]}>
+            Learn more about the artist and the inspiration behind his work
+          </p>
+          <button
+            onClick={() => props.handlePageChange("About Me")}
+            className={styles["landing-page-card-button"]}
+          >
+            <AboutButton />
+          </button>
         </div>
-    )
-}
+      </div>
+
+      {/*Portfolio component*/}
+      <div className={styles["landing-page-card"]}>
+        <div style={{ zIndex: "2" }}>
+          <p className={styles["landing-page-paragraph"]}>
+            {" "}
+            Check out pieces done using a mix of charcoal, and digital media.{" "}
+          </p>
+          <button
+            onClick={() => props.handlePageChange("Portfolio")}
+            className={styles["landing-page-card-button"]}
+          >
+            <section className={"gradient-pink-dark-blue"}>Portfolio</section>
+          </button>
+        </div>
+        <img style={{ margin: "1em" }} src={PortfolioPic} alt="" />
+      </div>
+
+      {/*shopping component*/}
+
+      <div className={styles["landing-page-card"]}>
+        <img src={tShirtPic} alt="" />
+        <div style={{ margin: "1em", zIndex: "2" }}>
+          <p className={styles["landing-page-paragraph"]}>
+            Shop prints, stickers, clothing inquire about custom pieces!
+          </p>
+          <button
+            onClick={() => props.handlePageChange("Shopping")}
+            className={styles["landing-page-card-button"]}
+          >
+            <section className={"gradient-green-light-blue"}>SHOP</section>
+          </button>
+        </div>
+      </div>
+
+      {/*----------------------------------------------------------------------------------------*/}
+      {/*                       landing page blob elements position absolute                     */}
+      {/*----------------------------------------------------------------------------------------*/}
+
+      <div className={styles["landing-page-blob-1"]}></div>
+      <div className={styles["landing-page-blob-2"]}></div>
+      <div className={styles["landing-page-blob-3"]}></div>
+      <div className={styles["landing-page-blob-4"]}></div>
+      <div className={styles["landing-page-blob-5"]}></div>
+      <div className={styles["landing-page-blob-6"]}></div>
+    </div>
+  );
+};
 export default LandingPage;
